2ef1fda074536e6bfd966aa4f1b83b06239fa704
shix <15604215906@163.com>
2025-06-11 17:23:39 +0800

    增加uselayouteffect例子

--- hooks/src/demo/useLayoutEffect例子.tsx
+++ hooks/src/demo/useLayoutEffect例子.tsx
@@ -3,16 +3,16 @@ import { useLayoutEffect } from "react"
 function App() {
   const scrollHandler = (e:React.UIEvent<HTMLDivElement>) => {
     console.log('scroll')
-    const scrollTop =e.currentTarget.scrollTop
+    const scrollTop = e.currentTarget.scrollTop
     window.history.replaceState({}, '', `?top=${scrollTop}`)
   }
-  useLayoutEffect(()=>{
+  useLayoutEffect(() => {
     const containter = document.getElementById('containter') as HTMLDivElement
     const top = window.location.search.split('=')[1]
-    containter.scrollTop = Number(top)||0
-  },[])
+    containter.scrollTop = Number(top) || 0
+  }, [])
   return (
-    <div onScroll={scrollHandler} id="containter" style={{height:'500px',overflow:'auto'}}>
+    <div onScroll={scrollHandler} id="containter" style={{height:'500px', overflow:'auto'}}>
       {
         Array.from({ length: 500 }).map((_, index) => {
           return <div key={index}>{index + 1}</div>
